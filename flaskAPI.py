3f5cffc9bdbec21e2090a32c103f97594a6056f3
Jacob Chamie <jacobchamie@Jacobs-MBP.localdomain>
2021-12-28 09:45:40 -0800

    added files

---
+++ flaskAPI.py
@@ -0,0 +1,31 @@
+import flask
+from flask import request, jsonify, render_template
+import sys
+import argparse
+import json
+import os
+from importlib.resources import read_text
+from cheggscraper import Downloader
+
+
+app = flask.Flask(__name__)
+link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-integration-2x-4y-1-da-bounded-y-x-2-y-x-3-evaluate-double-integrals-q5681991'
+app.config["DEBUG"] = True
+
+conf = json.loads(read_text('cheggscraper', 'conf.json'))
+default_save_file_format = conf.get('default_save_file_format')
+default_cookie_file_path = conf.get('default_cookie_file_path')
+
+@app.route('/')
+def my_form():
+    return "home page"
+
+@app.route('/get', methods=['GET'])
+def getAnswer():
+    input_json = request.args.get('link')
+    input_json = str(input_json)
+    print("link requested is " + input_json)
+    Downloader.main(input_json)
+    return "success"
+
+app.run()
