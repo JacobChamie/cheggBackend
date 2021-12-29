cda437044aa2e78793ab5ead98b271c2bd16e8da
Jacob Chamie <jacobchamie@Jacobs-MBP.localdomain>
2021-12-28 16:50:42 -0800

    Grwgraw

--- flaskAPI.py
+++ flaskAPI.py
@@ -5,7 +5,7 @@ import json
 import os
 from importlib.resources import read_text
 from cheggscraper import Downloader
-
+import logging
 
 app = Flask(__name__, template_folder='')
 link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-integration-2x-4y-1-da-bounded-y-x-2-y-x-3-evaluate-double-integrals-q5681991'
@@ -13,6 +13,9 @@ link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-in
 conf = json.loads(read_text('cheggscraper', 'conf.json'))
 default_save_file_format = conf.get('default_save_file_format')
 default_cookie_file_path = conf.get('default_cookie_file_path')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def my_form():
