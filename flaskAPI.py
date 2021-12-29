8b111bec5f207a1b4dc1479fc130b94141bbabc3
Jacob Chamie <jacobchamie@Jacobs-MBP.localdomain>
2021-12-28 16:23:35 -0800

    big changes

--- flaskAPI.py
+++ flaskAPI.py
@@ -7,7 +7,7 @@ from importlib.resources import read_text
 from cheggscraper import Downloader
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='')
 link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-integration-2x-4y-1-da-bounded-y-x-2-y-x-3-evaluate-double-integrals-q5681991'
 
 conf = json.loads(read_text('cheggscraper', 'conf.json'))
