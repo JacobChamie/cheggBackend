ffc77d04faa065f14731ad22cb977dd8cf84799b
Jacob Chamie <jacobchamie@Jacobs-MBP.localdomain>
2021-12-28 23:51:35 -0800

    grgaw

--- cheggscraper/Downloader.py
+++ cheggscraper/Downloader.py
@@ -13,21 +13,21 @@ def main(url):
     default_save_file_format = conf.get('default_save_file_format')
     default_cookie_file_path = conf.get('default_cookie_file_path')
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-c', '--cookie', default=default_cookie_file_path,
-                    help='path of cookie life', dest='cookie_file')
-    ap.add_argument('-u', '--url', help='url of chegg homework-help, put inside " "',
-                    type=str, dest='url')
-    ap.add_argument('-s', '--save',
-                    help='file path, where you want to save, put inside " " eg: test.html or'
-                         ' D:\\myFolder\\test.html or /home/test.html',
-                    type=str, default=default_save_file_format, dest='file_format')
-    args = vars(ap.parse_args())
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument('-c', '--cookie', default=default_cookie_file_path,
+    #                 help='path of cookie life', dest='cookie_file')
+    # ap.add_argument('-u', '--url', help='url of chegg homework-help, put inside " "',
+    #                 type=str, dest='url')
+    # ap.add_argument('-s', '--save',
+    #                 help='file path, where you want to save, put inside " " eg: test.html or'
+    #                      ' D:\\myFolder\\test.html or /home/test.html',
+    #                 type=str, default=default_save_file_format, dest='file_format')
+    # args = vars(ap.parse_args())
 
-    if not os.path.exists(path=args['cookie_file']):
-        raise Exception(f'{args["cookie_file"]} does not exists')
-    Chegg = CheggScraper(cookie_path=args['cookie_file'])
-    Chegg.url_to_html(url, file_name_format=args['file_format'])
+    # if not os.path.exists(path=args['cookie_file']):
+    #     raise Exception(f'{args["cookie_file"]} does not exists')
+    Chegg = CheggScraper(cookie_path=default_cookie_file_path)
+    Chegg.url_to_html(url, file_name_format=default_save_file_format)
 
     #if not args.get('url'):
         #args.update({'url': input('Enter url of the homework-help: ')})
