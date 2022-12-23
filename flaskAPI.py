2739094921fbf5ceaca6a11d3573d5ab95c3daec
JacobChamie <jchamie@berkeley.edu>
2022-12-23 13:42:31 -0800

    tried adding login

--- flaskAPI.py
+++ flaskAPI.py
@@ -5,7 +5,8 @@ from cheggscraper import Downloader
 import logging
 import sys
 from cheggscraper import *
-
+import flask_login
+import flask
 
 app = Flask(__name__, template_folder='templates')
 link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-integration-2x-4y-1-da-bounded-y-x-2-y-x-3-evaluate-double-integrals-q5681991'
@@ -17,30 +18,42 @@ conf = json.loads(read_text('cheggscraper', 'conf.json'))
 default_save_file_format = conf.get('default_save_file_format')
 default_cookie_file_path = conf.get('default_cookie_file_path')
 
+users = {'admin': {'password': 'admin'}}
 
 @app.route('/')
+@flask_login.login_required
 def my_form():
     return render_template('index.html')
 @app.route('/Page-1.html')
+@flask_login.login_required
 def buyPage():
     return render_template('Page-1.html')
-@app.route('/login.html')
+@app.route('/login.html', methods=['GET', 'POST'])
 def loginPage():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    if username in users and password == users[username]['password']:
+        return flask.redirect(flask.url_for('Page-1.html'))
+    else:
+        return render_template('login.html')
 @app.route('/urlBox')
+@flask_login.login_required
 def urlLink():
     url = request.args.get('urlBox')
     parseAnswer = Downloader.main(url)
     parseAnswer = str(parseAnswer)[10:]
     return render_template(parseAnswer)
 @app.route('/get', methods=['GET'])
+@flask_login.login_required
 def getAnswer():
     input_json = request.args.get('link')
     parseAnswer = Downloader.main(input_json)
     parseAnswer = str(parseAnswer)[10:]
     return render_template(parseAnswer)
-# @app.errorhandler(Exception)
-# def all_exception_handler(error):
-#     return "Website under maintenence :), please return to previous page", 500
-# app.logger.addHandler(logging.StreamHandler(sys.stdout))
-# app.logger.setLevel(logging.ERROR)
+@app.errorhandler(Exception)
+def all_exception_handler(error):
+    return "Website under maintenence :), please return to previous page", 500
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
