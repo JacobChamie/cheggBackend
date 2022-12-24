bfd85e38ecaf1ef3e7b29a00a3182bf1c93e53cd
JacobChamie <jchamie@berkeley.edu>
2022-12-24 12:47:26 -0800

    grwagaw

--- flaskAPI.py
+++ flaskAPI.py
@@ -5,19 +5,18 @@ from cheggscraper import Downloader
 import logging
 import sys
 from cheggscraper import *
-import flask_login
-import flask
+
 
 app = Flask(__name__, template_folder='templates')
 link = 'https://www.chegg.com/homework-help/questions-and-answers/find-region-integration-2x-4y-1-da-bounded-y-x-2-y-x-3-evaluate-double-integrals-q5681991'
 
 parseAnswer = ""
 
+
 conf = json.loads(read_text('cheggscraper', 'conf.json'))
 default_save_file_format = conf.get('default_save_file_format')
 default_cookie_file_path = conf.get('default_cookie_file_path')
 
-users = {'admin': {'password': 'admin'}}
 
 @app.route('/')
 def my_form():
@@ -25,16 +24,9 @@ def my_form():
 @app.route('/Page-1.html')
 def buyPage():
     return render_template('Page-1.html')
-# @app.route('/login.html', methods=['GET', 'POST'])
-# def loginPage():
-#     if request.method == 'GET':
-#         return render_template('login.html')
-#     username = request.form['username']
-#     password = request.form['password']
-#     if username in users and password == users[username]['password']:
-#         return flask.redirect(flask.url_for('Page-1.html'))
-#     else:
-#         return render_template('login.html')
+@app.route('/login.html')
+def loginPage():
+    return render_template('login.html')
 @app.route('/urlBox')
 def urlLink():
     url = request.args.get('urlBox')
@@ -47,3 +39,8 @@ def getAnswer():
     parseAnswer = Downloader.main(input_json)
     parseAnswer = str(parseAnswer)[10:]
     return render_template(parseAnswer)
+@app.errorhandler(Exception)
+def all_exception_handler(error):
+    return "Website under maintenence :), please return to previous page", 500
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
